8ae940d35a3fe3b3b6d536a869ed16a45ed85fab
misbashaikh2004 <misba6602@gmail.com>
2026-02-17 20:24:50 +0530

    crud

--- Teacher/src/main/java/com/security/demo/repository/StudentRepository.java
+++ Teacher/src/main/java/com/security/demo/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.security.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.security.demo.model.HoD;
@@ -11,7 +12,8 @@ import com.security.demo.model.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
-	List<Student> findByRoll(Integer roll);
+    /*~~(class org.openrewrite.java.tree.J$Erroneous cannot be cast to class org.openrewrite.java.tree.J$Assignment (org.openrewrite.java.tree.J$Erroneous and org.openrewrite.java.tree.J$Assignment are in unnamed module of loader 'app'))~~>*/@Query("SELECT s FROM Student s WHERE s.roll = :roll")
+    List<Student> findByRoll(Integer roll);
 
 	List<Student> findByUsername(String username);
 
